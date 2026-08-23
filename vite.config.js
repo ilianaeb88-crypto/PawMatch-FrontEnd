7d7d19d138b5889006911d42ee86e4bae56de814
ilianaeb88-crypto <ilianaeb88@gmail.com>
2026-08-22 19:04:13 -0500

    homepage

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,20 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        index: 'index.html',
+        adoptionChoice: 'adoption-choice.html',
+        petChoice: 'pet-choice.html',
+        catGender: 'cat-gender.html',
+        catAge: 'cat-age.html',
+        catWeight: 'cat-weight.html',
+        petGender: 'pet-gender.html',
+        petAge: 'pet-age.html',
+        petWeight: 'pet-weight.html',
+        dogBreed: 'dog-breed.html',
+      },
+    },
+  },
 })
